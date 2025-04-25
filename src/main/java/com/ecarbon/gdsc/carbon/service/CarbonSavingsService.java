ad7ea12ead5ab0e9354579989236981befa449ca
M1nKyu <01.minkyu@gmail.com>
2025-04-25 13:01:00 +0900

    feat: Add CarbonSavingsService

---
+++ src/main/java/com/ecarbon/gdsc/carbon/service/CarbonSavingsService.java
@@ -0,0 +1,24 @@
+package com.ecarbon.gdsc.carbon.service;
+
+import com.ecarbon.gdsc.carbon.dto.CarbonSavingsResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CarbonSavingsService {
+
+    // TODO: Build response
+    public Optional<CarbonSavingsResponse> getCarbonSavings(String url){
+        return null;
+    }
+
+    // TODO: Implement breakdown of carbon savings by element type
+
+    // TODO: Implement weekly savings graph data
+
+    // TODO: Implement image optimization result retrieval
+
+    // TODO: Implement user contribution analysis
+
+}
