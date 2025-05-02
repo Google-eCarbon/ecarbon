263be9e98797eb55d22970492f37131c60b1f5fd
M1nKyu <01.minkyu@gmail.com>
2025-05-02 20:02:31 +0900

    refactor: Add @Getter annotation to CustomOAuth2User

--- src/main/java/com/ecarbon/gdsc/auth/principal/CustomOAuth2User.java
+++ src/main/java/com/ecarbon/gdsc/auth/principal/CustomOAuth2User.java
@@ -1,5 +1,6 @@
 package com.ecarbon.gdsc.auth.principal;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
@@ -7,25 +8,12 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import java.util.Collection;
 import java.util.Map;
 
+@Getter
 @RequiredArgsConstructor
 public class CustomOAuth2User implements OAuth2User {
+
     private final Map<String, Object> attributes;
     private final Collection<? extends GrantedAuthority> authorities;
     private final String name;
-
-    @Override
-    public Map<String, Object> getAttributes() {
-        return attributes;
-    }
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return authorities;
-    }
-
-    @Override
-    public String getName() {
-        return name;
-    }
 
 }
