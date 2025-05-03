e700bc9c0f2cb35aeb118ed51e4dd2484748f986
M1nKyu <01.minkyu@gmail.com>
2025-05-03 16:41:26 +0900

    feat: Add AuthCallback page for handling Google OAuth2 callback

--- frontend/my-dashboard/src/App.tsx
+++ frontend/my-dashboard/src/App.tsx
@@ -5,9 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Guidelines from "./pages/Guidelines";
-import Stats from "./pages/stats";
+import Stats from "./pages/Stats";
 import CategoryStats from "./pages/Categorystats";
 import Home from "./pages/Home";
+import AuthCallback from "./pages/auth/AuthCallback";
 
 const queryClient = new QueryClient();
 
@@ -24,6 +25,7 @@ const App = () => {
             <Route path="/stats" element={<Stats />} />
             <Route path="/category-stats" element={<CategoryStats />} />
             <Route path="/carbon-analysis" element={<Index />} />
+            <Route path="/auth/callback" element={<AuthCallback />} />
             {/* <Route path="*" element={<NotFound />} /> */}
           </Routes>
         </BrowserRouter>
