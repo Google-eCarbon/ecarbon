66acae3f8e7caf443e0762323ca2b8a19c8c8ec0
M1nKyu <01.minkyu@gmail.com>
2025-05-05 15:06:55 +0900

    chore: Remove unused imports

--- src/main/java/com/ecarbon/gdsc/carbon/controller/CarbonAnalysisController.java
+++ src/main/java/com/ecarbon/gdsc/carbon/controller/CarbonAnalysisController.java
@@ -7,15 +7,12 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 
 @Slf4j
 @RequiredArgsConstructor
