21e632608697a11fbadd15ff6ca56a4c9a9374e5
M1nKyu <01.minkyu@gmail.com>
2025-05-12 18:20:16 +0900

    feat: Implement API endpoint to fetch map markers

--- src/main/java/com/ecarbon/gdsc/carbon/controller/HomeController.java
+++ src/main/java/com/ecarbon/gdsc/carbon/controller/HomeController.java
@@ -1,22 +1,50 @@
 package com.ecarbon.gdsc.carbon.controller;
 
+import com.ecarbon.gdsc.carbon.dto.GlobeHomeResponse;
 import com.ecarbon.gdsc.carbon.entity.WeeklyMeasurements;
+import com.ecarbon.gdsc.carbon.enums.PlaceCategory;
+import com.ecarbon.gdsc.carbon.service.GlobeHomeService;
 import com.ecarbon.gdsc.carbon.service.HomeService;
+import com.ecarbon.gdsc.carbon.util.DateUtil;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/")
 @Slf4j
 public class HomeController {
 
     private final HomeService homePageService;
+    private final GlobeHomeService globeHomeService;
+
+    @GetMapping
+    public ResponseEntity<GlobeHomeResponse> getMapMarker(
+            @RequestParam(required = false) String weekStartDate,
+            @RequestParam(defaultValue = "UNIVERSITY") PlaceCategory placeCategory){
+
+        try {
+            if (weekStartDate == null || weekStartDate.isBlank()) {
+                weekStartDate = DateUtil.getWeeksMonday();
+            }
+
+            Optional<GlobeHomeResponse> responseOpt = globeHomeService.getGlobeHome(weekStartDate, placeCategory);
+
+            return responseOpt
+                    .map(ResponseEntity::ok)
+                    .orElseGet(() -> ResponseEntity.noContent().build()); // 결과 없음 처리
+
+        } catch (InterruptedException | ExecutionException e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build(); // 서버 에러 처리
+        }
+    }
 
     @PostMapping("/api/start-analysis")
     public ResponseEntity<String> startAnalysis(@RequestParam String url, HttpSession session){
