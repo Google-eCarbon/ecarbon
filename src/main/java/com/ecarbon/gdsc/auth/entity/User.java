b2bc4421db47b1766b8f574e37e5b9483f4ec4c2
M1nKyu <01.minkyu@gmail.com>
2025-05-12 19:55:26 +0900

    feat: Add user level, total reduction bytes and count, and measurement logs to User entity

--- src/main/java/com/ecarbon/gdsc/auth/entity/User.java
+++ src/main/java/com/ecarbon/gdsc/auth/entity/User.java
@@ -1,23 +1,33 @@
 package com.ecarbon.gdsc.auth.entity;
 
+import com.ecarbon.gdsc.carbon.entity.WeeklyMeasurements;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+import com.google.cloud.firestore.annotation.DocumentId;
+
+import java.util.List;
 
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-@Document(collection = "users")
 public class User {
-    @Id
+
+    @DocumentId
     private String id;
 
     private String email;
 
     private String name;
+
+    private int level;
+
+    private Long total_reduction_bytes;
+
+    private Long total_reduction_count;
+
+    private List<WeeklyMeasurements> user_measurement_logs;
 
 }
