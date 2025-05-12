7f9680099415e131dd0cbde8aa5b09099b9e796b
M1nKyu <01.minkyu@gmail.com>
2025-05-12 18:20:51 +0900

    feat: Create DTOs for map marker and response

---
+++ src/main/java/com/ecarbon/gdsc/carbon/dto/GlobeHomeResponse.java
@@ -0,0 +1,17 @@
+package com.ecarbon.gdsc.carbon.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Builder
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class GlobeHomeResponse {
+    private List<EmissionMapMarker> emissionMapMarkers;
+}
+
