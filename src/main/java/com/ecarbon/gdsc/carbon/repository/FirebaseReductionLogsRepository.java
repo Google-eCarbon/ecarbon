0cc5e2f1f11501ead5638b0d8ec4ab23f63cee24
M1nKyu <01.minkyu@gmail.com>
2025-05-13 17:19:44 +0900

    feat: Add findAllByEmail() to FirebaseReductionLogsRepository

--- src/main/java/com/ecarbon/gdsc/carbon/repository/FirebaseReductionLogsRepository.java
+++ src/main/java/com/ecarbon/gdsc/carbon/repository/FirebaseReductionLogsRepository.java
@@ -9,6 +9,7 @@ import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
@@ -33,5 +34,23 @@ public class FirebaseReductionLogsRepository {
         }
         // 첫 번째 문서를 ReductionLogs 객체로 변환
         return documents.get(0).toObject(ReductionLogs.class);
+    }
+
+    public List<ReductionLogs> findAllByEmail(String email) throws ExecutionException, InterruptedException {
+        Firestore db = FirestoreClient.getFirestore();
+
+        // 'id' 필드가 주어진 email과 같은 모든 문서를 조회
+        Query query = db.collection(COLLECTION_NAME)
+                .whereEqualTo("id", email);
+
+        ApiFuture<QuerySnapshot> future = query.get();
+        List<QueryDocumentSnapshot> documents = future.get().getDocuments();
+
+        // 결과가 없을 경우 빈 리스트 반환
+        List<ReductionLogs> results = new ArrayList<>();
+        for (QueryDocumentSnapshot document : documents) {
+            results.add(document.toObject(ReductionLogs.class));
+        }
+        return results;
     }
 }
